3c275abf2517cb06ee1239e7254f2a7a16b7df10
ANTON <19111984@list.ru>
2022-08-23 18:50:25 +0700

    ChangePosNeg

---
+++ Task32/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу замена элементов 
+массива: положительные элементы замените на 
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+Console.Clear();
+int[] CreateArrayRndInt(int size, int min, int max)// метод создания массива (размер, мин, макс число интервала)
+{// начало тела метода
+int[] array = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < size; i++)//цикл расчета значений массива по индексу о "0" до "размера"
+{
+    array[i] = rnd.Next(min, max+1);//расчет значений массива по индексу о "0" до "размера"
+}
+return array;//возваращение в начало цикла
+}// конец тела метода
+
+void PrintArray(int[] array)// метод вывода массива на консоль
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length-1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+void ChangePoseOnNegative(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    array[i]*= -1;
+    }
+}
+int[] arr = CreateArrayRndInt(4, -10, 10);
+PrintArray(arr);
+ChangePoseOnNegative(arr);
+PrintArray(arr);
